35c7518c43cb26c98f1c34cfe2ee132e423a14f1
bestzagreb71-coder <bestzagreb71@gmail.com>
2026-09-25 05:23:20 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'iz-tribine-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './podaci.js',
+  './manifest.json',
+  './logo.png'
+];
+
+// Instalacija Service Workera i spremanje osnovnih datoteka u memoriju
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Dohvaćanje sadržaja (omogućuje rad i bez interneta za već učitane stvari)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
